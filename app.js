7a1e1db3c4c28a2f5d22c7b66793da88412c86bc
zaidahmed6500 <zaidahmed6500@gmail.com>
2025-09-20 14:05:19 +0500

    changes

--- app.js
+++ app.js
@@ -14,9 +14,25 @@ function formatDate(date){
     );
 }
 
-const formatted = new Date("2025-08-09")
-console.log(formatDate(formatted))
+const format = new Date("2025-08-09")
+console.log(formatDate(format))
 
 
 
-// Assignment 2
+// Assignment 3
+function calculateBirthday(){
+    let currentDate = new Date().getTime();
+let birthDate = new Date("September 13, 2007 13:25:00").getTime();
+let remaining = currentDate - birthDate;
+let calculateAge = Math.floor(remaining / 1000 / 60 / 60 / 24 / 365);
+let calculateDays = Math.floor(remaining / 1000 / 60 / 60 / 24);
+console.log(calculateAge)
+console.log(calculateDays)
+}
+
+calculateBirthday();
+
+
+
+
+// assignment
